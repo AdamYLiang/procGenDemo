61e6e4e6803f2cc46de7591826b12e5710ad88bf
unknown <ayl292@nyu.edu>
2016-03-21 15:26:43 -0400

    fixed to correct proc gen TODO:Finish restart/random

--- procGenLab/Assets/Scripts/Pathmaker.cs
+++ procGenLab/Assets/Scripts/Pathmaker.cs
@@ -4,8 +4,11 @@ using System.Collections;
 public class Pathmaker : MonoBehaviour {
 
 	private int counter = 0;
-	public Transform floorPrefab; 
+	public Transform floorPrefab1; 
+	public Transform floorPrefab2; 
+	public Transform floorPrefab3; 
 	public Transform pathmakerPrefab;
+	Transform tempFloor;
 
 	// Use this for initialization
 	void Start () {
@@ -16,8 +19,8 @@ public class Pathmaker : MonoBehaviour {
 	void Update () {
 
 		if(counter < 50){
+			//Gen for pathmaker and rotates
 			float randomNumber = Random.Range(0f,1f);
-
 			if(randomNumber < 0.25f){
 				transform.Rotate(new Vector3(0, 90, 0));
 			}
@@ -29,9 +32,24 @@ public class Pathmaker : MonoBehaviour {
 			if(randomNumber >= 0.99f && randomNumber <= 1f){
 				Object clone = Instantiate(pathmakerPrefab, transform.position, Quaternion.identity);
 				//transform.position += transform.forward * Time.deltaTime * 5;
-				transform.position += transform.forward * 5;
-				counter++;
 			}
+
+			//Choosing what tile to spawn
+			randomNumber = Random.Range(0f,1f);
+			if(randomNumber <= 0.20f){
+				tempFloor = floorPrefab1;
+			}
+			else if(randomNumber > 0.20f && randomNumber <= 0.60F){
+				tempFloor = floorPrefab2;
+			}
+			else if(randomNumber > 0.60F){
+				tempFloor = floorPrefab3;
+			}
+			//Choosing height to spawn at
+
+			Object temp = Instantiate(tempFloor, transform.position, Quaternion.identity);
+			transform.position += transform.forward * 5;
+			counter++;
 
 		}
 	
